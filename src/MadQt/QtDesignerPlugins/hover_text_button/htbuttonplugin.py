b2c1a88aee9cbbd48978d7405b0145ccbfdbfe2e
Fabio Goncalves <30872066+MadPonyInteractive@users.noreply.github.com>
2022-01-06 14:54:16 +0000

    v0.0.29

---
+++ src/MadQt/QtDesignerPlugins/hover_text_button/htbuttonplugin.py
@@ -0,0 +1,86 @@
+"""
+Widget: HtButton
+Version: 0.0.1
+
+Contributors: Fabio Goncalves
+Email: fabiogoncalves@live.co.uk
+
+Description: A QPushButton with a hovered state and text
+"""
+import os
+CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
+
+# Importing from the MadQt package Widgets folder
+from MadQt.Widgets.hover_text_button.htbutton import HtButton
+
+from PySide6.QtCore import QObject
+from PySide6.QtGui import QIcon
+from PySide6.QtDesigner import (QExtensionManager,
+    QDesignerCustomWidgetInterface,
+    QDesignerFormWindowInterface)
+
+DOM_XML = """
+<ui language='c++'>
+    <widget class='HtButton' name='htbutton'/>
+</ui>
+"""
+
+class HtButtonPlugin(QObject, QDesignerCustomWidgetInterface):
+    def __init__(self):
+        QObject.__init__(self)
+        QDesignerCustomWidgetInterface.__init__(self)
+        self._form_editor = None
+
+    def createWidget(self, *args, **kwargs):
+        widget = HtButton(*args, **kwargs)
+        widget.defaultTextChanged.connect(self.defaultTextChanged)
+        widget.hoverStateChanged.connect(self.hoverStateChanged)
+        return widget
+
+    def domXml(self):
+        return DOM_XML
+
+    def group(self):
+        return 'Buttons'
+
+    def icon(self):
+        imgLocation = os.path.join(CURRENT_DIR,'htbutton.ico')
+        return QIcon(imgLocation)
+
+    def includeFile(self):
+        # Importing from the MadQt package Widgets folder
+        return 'MadQt.Widgets.hover_text_button.htbutton'
+
+    def initialize(self, form_editor):
+        self._form_editor = form_editor
+
+    def isContainer(self):
+        return False
+
+    def isInitialized(self):
+        return self._form_editor is not None
+
+    def name(self):
+        return 'HtButton'
+
+    def toolTip(self):
+        return 'A button that changes text when hovered'
+
+    def whatsThis(self):
+        return self.toolTip()
+
+    def defaultTextChanged(self, text):
+        widget = self.sender()
+        if not widget or not isinstance(widget, HtButton):return
+        if not widget.isWidgetType():return
+        form = QDesignerFormWindowInterface.findFormWindow(widget)
+        if form:
+            form.cursor().setWidgetProperty(widget, "text", text)
+
+    def hoverStateChanged(self, state):
+        widget = self.sender()
+        if not widget or not isinstance(widget, HtButton):return
+        if not widget.isWidgetType():return
+        form = QDesignerFormWindowInterface.findFormWindow(widget)
+        if form:
+            form.cursor().setWidgetProperty(widget, "hovered", state)
---
+++ src/MadQt/Widgets/hover_text_button/htbutton.py
@@ -0,0 +1,73 @@
+"""
+Widget: HtButton
+Version: 0.0.1
+
+Contributors: Fabio Goncalves
+Email: fabiogoncalves@live.co.uk
+
+Description: A QPushButton with a hovered state and text
+"""
+from PySide6.QtCore import Signal, Slot, QSize, Property
+from PySide6.QtWidgets import QPushButton, QApplication
+
+class HtButton(QPushButton):
+    hoveredTextChanged = Signal(str)
+    defaultTextChanged = Signal(str)
+    hoverStateChanged = Signal(bool)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._hovered = False
+        self._defaultText = 'Hover Me'
+        self._hoveredText = 'Hello'
+        self.setText(self._defaultText)
+
+    @Slot(bool)
+    def setHovered(self, new_hovered):
+        self._hovered = new_hovered
+
+    @Slot(str)
+    def setHoveredText(self, new_hoveredText):
+        self._hoveredText = new_hoveredText
+        self.hoveredTextChanged.emit(new_hoveredText)
+
+    @Slot(str)
+    def setDefaultText(self, new_defaultText):
+        self._defaultText = new_defaultText
+        self.defaultTextChanged.emit(new_defaultText)
+
+    def getHovered(self):
+        return self._hovered
+
+    def getHoveredText(self):
+        return self._hoveredText
+
+    def getDefaultText(self):
+        return self._defaultText
+
+    def minimumSizeHint(self):
+        return QSize(75, 24)
+
+    def sizeHint(self):
+        return QSize(75, 24)
+
+    def enterEvent(self,event):
+        self.setText(self._hoveredText)
+        self._hovered = True
+        self.hoverStateChanged.emit(True)
+
+    def leaveEvent(self,event):
+        self.setText(self._defaultText)
+        self._hovered = False
+        self.hoverStateChanged.emit(False)
+
+    # hovered = Property(bool, getHovered, setHovered)
+    defaultText = Property(str, getDefaultText, setDefaultText)
+    hoveredText = Property(str, getHoveredText, setHoveredText)
+    hovered = Property(bool, getHovered, setHovered)
+
+if __name__ == '__main__':
+    import sys
+    app = QApplication(sys.argv)
+    window = HtButton()
+    window.show()
+    sys.exit(app.exec())
